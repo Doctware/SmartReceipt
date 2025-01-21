304c402c3110097cd412535fdfe9e9b5b0cc1cb0
Doctware <yusclever9@gmail.com>
2025-01-21 11:18:34 +0100

    dibbuging issues

--- backend/models/receipt.py
+++ backend/models/receipt.py
@@ -9,41 +9,38 @@ class Receipt(db.Model):
     """ the receipt model """
     __tablename__ = 'receipts'
 
-    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(db.String(36), primary_key=True, default=lambda: (uuid.uuid4()))
     item_name = db.Column(db.String(120), nullable=False)
-    amount = db.Column(db.Integer, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(200), nullable=True)
     date_sold = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     business_name = db.Column(db.String(120), nullable=True)
     address = db.Column(db.String(250), nullable=False)
     access_code = db.Column(db.String(8), unique=True, nullable=False)
     buyer_name = db.Column(db.String(120), nullable=False)
-    buyer_signature = db.Column(db.String(120), nullable=True)
+    buyer_signature = db.Column(db.Text, nullable=True)  # Use Text for base64 string
     locked = db.Column(db.Boolean, default=False, nullable=False)
-    buyer_signature = db.Column(db.Text, nullable=True)  # Change to Text to store the full base64 string
-
-
     seller_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
 
     def __init__(
-            self, item_name, amount, description,date_sold, buyer_name, address, seller_id, business_name=None
+            self, item_name, amount, description, buyer_name, address, seller_id, buyer_signature, date_sold=None, business_name=None
             ):
-        """ insialzing provided input """
-
+        """ initializing provided input """
         self.item_name = item_name
         self.amount = amount
         self.description = description
-        self.date_sold = date_sold
-        buyer_name=buyer_name
-        self.address= address
+        self.date_sold = date_sold or datetime.utcnow()
+        self.buyer_name = buyer_name
+        self.address = address
         self.seller_id = seller_id
-        self.businaess_name = business_name
+        self.business_name = business_name
+        self.buyer_signature = buyer_signature
         self.access_code = self.generate_access_code()
 
     def generate_access_code(self):
-        """ Generate a unique 8-caharcter access code. """
+        """ Generate a unique 8-character access code. """
         return str(uuid.uuid4())[:8]
 
     def lock_receipt(self):
-        """ locking receipt to prevent futher editing """
+        """ Locking receipt to prevent further editing """
         self.locked = True
